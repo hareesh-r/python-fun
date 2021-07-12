a7e4f837254cdc74b06268c711c6c54615cf5ebb
HAREESH <hareeshprogrammer@gmail.com>
2021-07-13 00:38:39 +0530

    Auto import

--- GUI boilerplate.py
+++ GUI boilerplate.py
@@ -1,10 +1,20 @@
-from tkinter import *
-import tkinter.font as font
+import os
+try:
+    from tkinter import *
+except:
+    os.system('pip -m install tkinter')
+
+try:
+    import tkinter.font as font
+except:
+    os.system('pip -m install tkinter.font')
+
 
 def GUI_getinput(test_string):
     arr = []
+
     def func():
-        string  = str(reg.get())
+        string = str(reg.get())
         arr.append(string)
         screen.destroy()
 
@@ -19,19 +29,22 @@ def GUI_getinput(test_string):
 
     myFont = font.Font(weight="bold")
 
-    Label(text = test_string  , fg="white" ,font = myFont, bg = "dodger blue" , width = "150" , height = "3" ).pack()
+    Label(text=test_string, fg="white", font=myFont,
+          bg="dodger blue", width="150", height="3").pack()
 
-    reg = Entry(screen,text = "Regulation Year",width="50",justify=CENTER)
+    reg = Entry(screen, text="Regulation Year", width="50", justify=CENTER)
 
     reg.focus_set()
-    
-    reg.pack(pady = "10" , padx = "10",ipady=5)
 
-    btn = Button(text = "Enter",width="15",fg="grey17",bg = "white", command = func)
+    reg.pack(pady="10", padx="10", ipady=5)
+
+    btn = Button(text="Enter", width="15",
+                 fg="grey17", bg="white", command=func)
 
     btn.pack(ipady=5)
 
     screen.mainloop()
     return arr[-1]
+
 
 print(GUI_getinput("Enter regulation 2017 or 2013 for"))
