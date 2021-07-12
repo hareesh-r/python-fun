d15dabc8616e67d6e1e9482170b1b5a2961550fc
HAREESH <hareeshprogrammer@gmail.com>
2021-07-13 00:40:27 +0530

    Import auto

--- Pencil with Turtle.py
+++ Pencil with Turtle.py
@@ -1,26 +1,34 @@
-import turtle
+try:
+    import turtle
+except:
+    import os
+    os.system('pip -m install trutle')
+    import turtle
+screen = turtle.Screen()
 
-screen= turtle.Screen()
-
-har=turtle.Turtle()
+har = turtle.Turtle()
 har.speed(-1)
 
-def drag(x,y):
+
+def drag(x, y):
     har.ondrag(None)
-    har.setheading(har.towards(x,y))
-    har.goto(x,y)
+    har.setheading(har.towards(x, y))
+    har.goto(x, y)
     har.ondrag(drag)
 
-def cr(x,y):
+
+def cr(x, y):
     har.clear()
+
 
 def main():
     turtle.listen()
 
     har.ondrag(drag)
 
-    turtle.onscreenclick(cr,3)
+    turtle.onscreenclick(cr, 3)
 
     screen.mainloop()
-main()
 
+
+main()
